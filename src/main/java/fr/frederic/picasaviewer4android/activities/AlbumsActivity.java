6f3e8eb7779bff2ce8de04e8fbcea4fd39a86258
frederic.minatchy <frederik78@yahoo.fr>
2014-02-02 10:03:43 +0100

    Renommage de MainActivity en AlbumsActivity

--- src/main/java/fr/frederic/picasaviewer4android/activities/AlbumsActivity.java
+++ src/main/java/fr/frederic/picasaviewer4android/activities/AlbumsActivity.java
@@ -1,6 +1,9 @@
-package fr.frederic.picasaviewer4android;
+package fr.frederic.picasaviewer4android.activities;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,13 +11,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.frederic.picasaviewer4android.Album;
+import fr.frederic.picasaviewer4android.ListAlbumsAdapter;
+import fr.frederic.picasaviewer4android.R;
+
+public class AlbumsActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final List<Album> albums = new ArrayList<Album>();
+        final Drawable drawable = getResources().getDrawable(R.drawable.ic_launcher);
+        for(int i =0; i < 5; i++)
+        {
+            albums.add(new Album("album_" +i, drawable));
+        }
+        this.setListAdapter(new ListAlbumsAdapter(this, albums));
 
     }
 
