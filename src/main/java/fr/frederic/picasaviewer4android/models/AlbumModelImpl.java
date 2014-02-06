5b34a3f0a499ec11300ba40c89008dea994c3d1b
Frédéric Minatchy <frederic.minatchy@gmail.com>
2014-02-06 15:04:46 +0100

    Ajout de l'annotation @Singleton sur AlbumModelImpl

--- src/main/java/fr/frederic/picasaviewer4android/models/AlbumModelImpl.java
+++ src/main/java/fr/frederic/picasaviewer4android/models/AlbumModelImpl.java
@@ -1,7 +1,10 @@
 package fr.frederic.picasaviewer4android.models;
 
+import com.google.inject.Singleton;
+
 /**
  * Created by Frederic on 03/02/14.
  */
+@Singleton
 public class AlbumModelImpl extends AbstractAlbumModel{
 }
