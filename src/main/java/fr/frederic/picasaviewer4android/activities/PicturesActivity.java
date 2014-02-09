2266311bbf5ef49b8f95bbf2153a1fb5c575215b
frederic.minatchy <frederik78@yahoo.fr>
2014-02-09 22:10:46 +0100

    Implémenation de onCreate de PicturesActivity

--- src/main/java/fr/frederic/picasaviewer4android/activities/PicturesActivity.java
+++ src/main/java/fr/frederic/picasaviewer4android/activities/PicturesActivity.java
@@ -1,11 +1,18 @@
 package fr.frederic.picasaviewer4android.activities;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 import com.google.inject.Inject;
 
+import java.util.List;
+
 import fr.frederic.picasaviewer4android.R;
+import fr.frederic.picasaviewer4android.lists.GridPictureAdapter;
 import fr.frederic.picasaviewer4android.models.pictures.PicturesModel;
+import fr.frederic.picasaviewer4android.vos.Album;
+import fr.frederic.picasaviewer4android.vos.Picture;
 import roboguice.activity.RoboActivity;
 
 /**
@@ -21,6 +28,12 @@ public class PicturesActivity extends RoboActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.picture);
-//        picturesModel.getAllPictures()
+        final Bundle bundle = getIntent().getExtras();
+        if(bundle != null && bundle.get("album") != null)
+        {
+            final List<Picture> pictures = picturesModel.getAllPictures((Album) bundle.get("album"));
+            final GridView gridView = (GridView) findViewById(R.id.gridview);
+            gridView.setAdapter(new GridPictureAdapter(this, pictures));
+        }
     }
 }
