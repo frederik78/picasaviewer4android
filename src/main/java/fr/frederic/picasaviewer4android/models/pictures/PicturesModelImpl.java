6efc185dd6a1cc457f9cadba3303c8d46c313240
frederic.minatchy <frederik78@yahoo.fr>
2014-02-16 14:27:06 +0100

    Renommage de PictureModelImpl en PicturesModelImpl

--- src/main/java/fr/frederic/picasaviewer4android/models/pictures/PicturesModelImpl.java
+++ src/main/java/fr/frederic/picasaviewer4android/models/pictures/PicturesModelImpl.java
@@ -3,5 +3,5 @@ package fr.frederic.picasaviewer4android.models.pictures;
 /**
  * Created by Frederic on 08/02/14.
  */
-public class PictureModelImpl extends AbstractPicturesModel {
+public class PicturesModelImpl extends AbstractPicturesModel {
 }
