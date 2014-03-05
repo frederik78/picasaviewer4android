12c0856122781ef3326048c30d0d2e0f438e0998
frederic.minatchy <frederik78@yahoo.fr>
2014-03-05 21:12:01 +0100

    Ajout de la constante APPLICATION_NAME

---
+++ src/main/java/fr/frederic/picasaviewer4android/util/Constantes.java
@@ -0,0 +1,10 @@
+package fr.frederic.picasaviewer4android.util;
+
+/**
+ * Created by Frederic on 03/03/14.
+ */
+public class Constantes {
+
+    public static final String APPLICATION_NAME = "picasaviewer4android";
+
+}
