516bb41d69c87740f060ab972d180f576884ef70
frederic.minatchy <frederik78@yahoo.fr>
2014-03-12 22:30:29 +0100

    Ajout de PicasaWebServiceProvider dans AlbumModule

--- src/main/java/fr/frederic/picasaviewer4android/modules/AlbumModule.java
+++ src/main/java/fr/frederic/picasaviewer4android/modules/AlbumModule.java
@@ -4,36 +4,33 @@ import com.google.gdata.client.photos.PicasawebService;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provider;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import fr.frederic.picasaviewer4android.models.albums.AlbumsModel;
 import fr.frederic.picasaviewer4android.models.albums.AlbumsModelImpl;
 import fr.frederic.picasaviewer4android.util.Constantes;
-import fr.frederic.picasaviewer4android.util.PicasaHelper;
 
 /**
  * Created by Frederic on 04/02/14.
  */
 public class AlbumModule extends AbstractModule {
 
+    Provider<PicasawebService> picasawebServiceProvider = new Provider<PicasawebService>()
+    {
+        @Override
+        public PicasawebService get() {
+            return new PicasawebService(Constantes.APPLICATION_NAME);
+        }
+    };
+
+
     @Override
     protected void configure() {
-//        bind(URL.class).toProvider(urlProvider);
+        bind(PicasawebService.class).toProvider(picasawebServiceProvider);
         bind(AlbumsModel.class).to(AlbumsModelImpl.class);
 
     }
 
 
-//    final Provider<URL> urlProvider = new Provider<URL>(){
-//        @Override
-//        public URL get() {
-//            try {
-//                return new URL("https://picasaweb.google.com/data/feed/api/user/" + "frederic.minatchy" + "?kind=album");
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            }
-//           return null;
-//        }
-//    };
 }
+
+
+
