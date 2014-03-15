d2cc4d0f6eb093f02838773e0d37b96c5930b2a1
frederic.minatchy <frederik78@yahoo.fr>
2014-03-15 21:36:43 +0100

    Nettoyage test unitaire

--- src/test/java/fr/frederic/picasaviewer4android/models/albums/TestAlbumsModel.java
+++ src/test/java/fr/frederic/picasaviewer4android/models/albums/TestAlbumsModel.java
@@ -64,12 +64,10 @@ public class TestAlbumsModel {
     AlbumsModel albumsModel;
 
     @Mock
-    PicasawebService picasawebService; // = mock(PicasawebService.class);
-
+    PicasawebService picasawebService;
 
     @Before
     public void init() throws TechnicalException {
-//        return Modules.override(roboGuiceModule).with(new AlbumModule());
         MockitoAnnotations.initMocks(this);
         Module module = new AbstractModule() {
             @Override
@@ -85,51 +83,20 @@ public class TestAlbumsModel {
         RoboGuice.setBaseApplicationInjector(Robolectric.application, RoboGuice.DEFAULT_STAGE, roboGuiceModule, module);
         RoboInjector injector = RoboGuice.getInjector(Robolectric.application);
         injector.injectMembers(this);
-        initAccounts();
     }
 
-    protected void initAccounts() throws TechnicalException {
-        creationComptes();
-        accountManager = AccountManager.get(Robolectric.application);
-        shadowOf(accountManager).addAccount(account0);
-        shadowOf(accountManager).addAccount(account1);
-        shadowOf(accountManager).addAccount(account2);
-    }
 
-    @Test
-    public void test_comptes() throws IOException, ServiceException, TechnicalException {
-//       final AlbumsActivity activity = Robolectric.buildActivity(AlbumsActivity.class).create().get();
-//
-//       final Account[] accounts = activity.getGoogleAccounts();
-//       assertThat(accounts).containsExactly(account0, account2);
-
-    }
 
     @Test
     public void test_obtenir_albums() throws ServiceException, ExecutionException, InterruptedException, IOException, TechnicalException {
 
-//        final AlbumsModel albumsModel = new AlbumsModelImpl("","");
-//        List<Album> albumList = albumsModel.getAllAlbums("default");
-
         final URL feedUrl = new URL("https://picasaweb.google.com/data/feed/api/user/" + "username" + "?kind=album");
         final UserFeed userFeed = mock(UserFeed.class);
-        final GphotoEntry gphotoEntry1 = spy(new GphotoEntry());
-        when(gphotoEntry1.getId()).thenReturn("idGPhoto1");
-        final GphotoEntry gphotoEntry2 = spy(new GphotoEntry());
-        when(gphotoEntry2.getId()).thenReturn("idGPhoto1");
-        final List<GphotoEntry> entries = Arrays.asList(new GphotoEntry[]{gphotoEntry1, gphotoEntry2});
+        final List<GphotoEntry> entries = Arrays.asList(new GphotoEntry[]{spy(new GphotoEntry()), spy(new GphotoEntry())});
         when(userFeed.getEntries()).thenReturn(entries);
         when(picasawebService.getFeed(any(feedUrl.getClass()), Matchers.eq(UserFeed.class))).thenReturn(userFeed);
         when(userFeed.getEntries()).thenReturn(entries);
         final List<Album> albums = ((AlbumsModelImpl) albumsModel).getAlbumsFromPicasa("username");
         assertThat(albums).hasSize(2);
     }
-
-
-    private void creationComptes() {
-        account0 = new Account("compte n°1", GoogleAccountManager.ACCOUNT_TYPE);
-        account1 = new Account("compte n°2", "pas google");
-        account2 = new Account("compte n°3", GoogleAccountManager.ACCOUNT_TYPE);
-    }
-
 }
